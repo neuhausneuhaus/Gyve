e6f24bf92c3d833eb881acafec27664325de5612
Neuhaus <neuhaus87@gmail.com>
2015-07-17 10:38:49 -0400

    created mvp donation create

---
+++ app/controllers/donations_controller.rb
@@ -0,0 +1,21 @@
+class DonationsController < ApplicationController
+
+  def create
+    donation = Donation.new
+      # binding.pry
+    if donation.update(new_donation_params)
+      # update donor's funds
+      binding.pry
+      # confirm donation, and new balance
+      redirect_to '/home'
+    end
+  end
+
+
+
+  private
+  def new_donation_params
+    params.permit(:amount, :donor_id, :client_id, :goal_id)
+  end
+
+end
