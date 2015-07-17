e3eb76784c44f4b51d0b657f9de7c91e41527e02
Neuhaus <neuhaus87@gmail.com>
2015-07-17 13:59:33 -0400

    added >0 constraint to funds remaining on donation create

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -3,13 +3,28 @@ class DonationsController < ApplicationController
   def create
     donation = Donation.new
 
-    if donation.update(new_donation_params)
-      # update donor's funds
-      binding.pry
-      # confirm donation, and new balance
-      redirect_to '/home'
-    end
+    #define donor and amounts
+    donor = Donor.find(params[:donor_id])
+    new_funds_remaining = donor.funds_remaining -= params[:amount].to_f
+    new_funds_spent = donor.funds_spent += params[:amount].to_f
     
+    #if donor has neccesary funds
+    if new_funds_remaining > 0 
+
+      #Try to create Donation
+      donor.update_attributes(:funds_remaining =>new_funds_remaining, :funds_spent => new_funds_spent)
+      if donation.update(new_donation_params)
+        # confirm donation, and new balance
+        redirect_to :back #change to on page confirmation text
+      else #if donation create fails
+      #raise error message
+      binding.pry
+      end
+
+    else #if donor does not have neccesary funds
+      #raise error message
+      binding.pry
+    end
   end
 
 
